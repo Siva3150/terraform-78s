ecb194f32c82e36a1329a5a65c47f59d30ef57c9
siva3150 <siva123@gmail.com>
2024-08-13 17:38:13 +0530

    terraform-78s

--- locals/count.tf
+++ locals/count.tf
@@ -1,20 +1,28 @@
-resource "aws_instance" "web"{
-    count = 11  # count.index is a special variable given by terraform
-    ami = var.ami_id
-    instance_type = local.instance_type
+# resource "aws_instance" "web"{
+#     count = 11  # count.index is a special variable given by terraform
+#     ami = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
+#     instance_type = local.instance_type
 
-    tags = {
-        Name = var.instance_names[count.index]
-    }
+#     tags = {
+#         Name = var.instance_names[count.index]
+#     }
 
-}
+# }
 
-resource "aws_route53_record" "www" {
-  count = 11
-  zone_id = var.zone_id
-  name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
-  type    = "A"
-  ttl     = 1
-  records = [local.ip]
+# resource "aws_route53_record" "www" {
+#   count = 11
+#   zone_id = var.zone_id
+#   name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
+#   type    = "A"
+#   ttl     = 1
+#   records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 
+# }
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id #devops-practice
+  instance_type = local.instance_type
+  tags = {
+    Name = "locals"
+  }
 }
