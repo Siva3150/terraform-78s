1859282ec31aa4f232c50b4fb835bdf91044759d
siva3150 <siva123@gmail.com>
2024-08-13 13:52:55 +0530

    terraform-78s

--- count/variables.tf
+++ count/variables.tf
@@ -4,5 +4,14 @@ variable "instance_names" {
 }
 
 variable "ami_id" {
+  type = string 
   default = "ami-0b4f379183e5706b9"
+}
+
+variable "zone_id" {
+    default = "Z00146592EVUBM8N30O7L"
+}
+
+variable "domain_name" {
+    deafult = "sivadevops.online"
 }
